e8ac7420353227ea4f5c1dbe5d7298fc547992a4
alexhoneker <84548429+alexhoneker@users.noreply.github.com>
2025-06-13 07:42:22 +0000

    Deploying to gh-pages from @ alexhoneker/alexhoneker.github.io@3168cc7755638e512fcd2e3e9270b0d079b262aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -480,7 +480,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%68%6F%6E%65%6B%65@%63%6C%65%6D%73%6F%6E.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -501,14 +501,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=O-Tvq1IAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
