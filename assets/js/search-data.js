8fde729cb303e8dbfb6c9750854e794cfb1aa986
alexhoneker <84548429+alexhoneker@users.noreply.github.com>
2025-06-13 08:52:46 +0000

    Deploying to gh-pages from @ alexhoneker/alexhoneker.github.io@7a7740890fe4619a6f5ee5569acb43f887199cd7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
